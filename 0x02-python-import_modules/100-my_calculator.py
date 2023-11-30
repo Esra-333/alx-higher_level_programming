d76e09f3f46e11425589bdde2d02c6f1312e9e52
Esra-333 <esra.omer444@gmail.com>
2023-11-30 08:11:16 -0800

    a

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
+    """Handle basic arithmetic operations.
+    Performs basic operations like addition, substraction,
+    multiplication and division between two numbers.
+    The program will execute an operation between two numbers
+    selected by the operator sent to the program.
+    """
+
     from calculator_1 import add, sub, mul, div
     import sys
 
