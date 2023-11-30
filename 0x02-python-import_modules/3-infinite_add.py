eb2d90ca0bd5a9ac31aa71e2e08e57e0cb780c26
Esra-333 <esra.omer444@gmail.com>
2023-11-30 03:58:11 -0800

    a

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def main():
-    sum = 0
-    for i in argv[1:]:
-        sum += int(i)
-    print("{:d}".format(sum))
-
 
 if __name__ == "__main__":
-    main()
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
