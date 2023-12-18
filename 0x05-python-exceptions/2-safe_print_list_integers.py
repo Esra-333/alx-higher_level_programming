df56e300024079a5610d1862867c2d41a42aae2d
Esra-333 <esra.omer444@gmail.com>
2023-12-18 01:23:27 -0800

    a

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
             temp += 1
         except (TypeError, ValueError):
             pass
-        print()
-        return temp
+        print("")
+        return (temp)
