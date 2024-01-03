c661a355e50cb464bed04a5d9c52411aa4955b97
Esra-333 <esra.omer444@gmail.com>
2024-01-03 02:40:40 -0800

    a

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 """Module for add_integer method."""
 
 
 def add_integer(a, b=98):
     """Adds two integers.
+
 
     Args:
      a: the first integer.
@@ -20,6 +22,7 @@ def add_integer(a, b=98):
     if type(b) not in (int, float):
         raise TypeError('b must be an integer')
     return int(a) + int(b)
+
 
 if __name__ == "__main__":
     import doctest
