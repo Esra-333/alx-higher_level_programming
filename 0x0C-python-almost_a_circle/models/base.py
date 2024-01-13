bcdfb15ccb9689ab50b227a42f005c9e0482f639
Esra-333 <esra.omer444@gmail.com>
2024-01-12 23:40:05 -0800

    a

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define Base class"""
+from os import path
+import json
+
+
+class Base(object):
+    """Base: Class define base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """__init__ initialized constructor
+        Args:
+         id (int): Defaults 2 None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
